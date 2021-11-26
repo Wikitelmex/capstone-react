ca26f3eb9d55a3314adb045f89b35e822b6eaa99
Alex Castillo <wikitelmex@gmail.com>
2021-11-26 16:25:35 -0700

    linters

--- src/__tests__/HomePage.test.js
+++ src/__tests__/HomePage.test.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import {
   BrowserRouter as Router,
 } from 'react-router-dom';
-import HomePage from '../Pages/HomePage';
+import HomePage from '../Pages/HomePage.jsx';
 
 import store from '../redux/store';
 
