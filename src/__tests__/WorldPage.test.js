288e55bbb597485a46736794ec6f1e5073c7b859
Alex Castillo <wikitelmex@gmail.com>
2021-11-26 16:56:23 -0700

    linters

--- src/__tests__/WorldPage.test.js
+++ src/__tests__/WorldPage.test.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import {
   BrowserRouter as Router,
 } from 'react-router-dom';
-import WorldPage from '../Pages/WorldPage';
+import WorldPage from '../Pages/WorldPage.js';
 
 import store from '../redux/store';
 
